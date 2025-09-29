95301b9e1dbe52342395a4e99b72048777016ab9
Ayush Shakya <ayshakya24@gmail.com>
2025-09-29 15:29:42 +0530

    Destructing, Rest, Spread

--- JS/jsdemo1.js
+++ JS/jsdemo1.js
@@ -1,4 +1,7 @@
-import {add,mult} from './jsdemo';
+// import {add,mult} from './jsdemo';
 
-console.log("sum=", add(34,45));
-console.log("mul=", mult(3,45));
+// console.log("sum=", add(34,45));
+// console.log("mul=", mult(3,45));
+
+const num = [1,2,3,4,5,6,7,8,9];
+const even = num.filter((n)=> (n%2==0)).map((n))
